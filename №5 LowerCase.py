500a90196a3e5d3ebadc3a698ad8e2f626e90dfb
Yur-ok <Materik-r@yandex.ru>
2016-04-28 18:14:03 +0700

    implements with re and own func

--- №5 LowerCase.py
+++ №5 LowerCase.py
@@ -3,6 +3,31 @@ import re
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzKTUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqnLxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIVjXapzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnWetekUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfCvzQWrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXbJrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkmjCCEUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnLfBtQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuTSkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCfKCuUJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWHuXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxgc'
 
 lUl = re.compile(r'([a-z]{1})[A-Z]+([a-z]{1})')
+lUl_2 = re.compile(r'[a-z]{1}[A-Z]+[a-z]{1}')
 
+lll = lUl.findall(line)
+
+print('Use RE')
 print(lUl.findall(line))
+print(len(lUl.findall(line))*2)
+print(lUl_2.findall(line))
+print('\n')
 
+def lowUlow(string):
+    p = 0
+    llist = []
+    for i in range(len(string)):
+        if p < 996:
+            p +=1
+        if string[i].islower() == string[p].islower():
+            continue
+        if string[i].islower():
+            llist.append(string[i])
+        else:
+            llist.append(string[p])
+    return llist
+
+
+print('Use own func')
+print(lowUlow(line))
+print(len(lowUlow(line)))
