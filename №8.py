5a44d99ac1fb3992c202de55a6b965aedaa98f9d
Yur-ok <Materik-r@yandex.ru>
2016-04-28 14:01:33 +0700

    first commit

---
+++ №8.py
@@ -0,0 +1,11 @@
+def sto():
+    for i in range(1,101):
+        if (i % 3) == 0:
+            i = 'Foo'
+        elif (i % 5) == 0:
+            i = 'Bar'
+        elif (i % 3) == 0 and (i % 5) == 0:
+            i = 'FooBar'
+        print(i)
+
+sto()
