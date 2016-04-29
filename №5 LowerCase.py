96986e207c0bdaa3a27557a61e340174919f6f4f
Yur-ok <Materik-r@yandex.ru>
2016-04-29 15:57:36 +0700

    reformat code for more readability

--- №5 LowerCase.py
+++ №5 LowerCase.py
@@ -8,18 +8,18 @@ lUl_2 = re.compile(r'[a-z]{1}[A-Z]+[a-z]{1}')
 lll = lUl.findall(line)
 
 print('Use RE')
-print(lUl.findall(line))
-print('Totoal number is: ',len(lUl.findall(line))*2, '\n')
 print(lUl_2.findall(line))
-print('\n')
+print(lUl.findall(line))
+print('Totoal number is: ', len(lUl.findall(line)) * 2, '\n')
+
 
 def lowUlow(string):
     p = 0
     llist = []
     for i in range(len(string)):
-        if p < len(string):
-            p +=1
-        if string[i].islower() == string[p].islower():
+        if p < len(string) - 1:
+            p += 1
+        if p < len(string) and string[i].islower() == string[p].islower():
             continue
         if string[i].islower():
             llist.append(string[i])
@@ -30,4 +30,4 @@ def lowUlow(string):
 
 print('Use my own function')
 print(lowUlow(line))
-print('Total number is: ',len(lowUlow(line)))
+print('Total number is: ', len(lowUlow(line)))
